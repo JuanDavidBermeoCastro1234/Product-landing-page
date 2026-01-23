6222c9fea1c95f5dd042c4f9639019bd2abe0d13
JuanDavidBermeoCastro <[186082659+JuanDavidBermeoCastro@users.noreply.github.com](mailto:186082659+JuanDavidBermeoCastro@users.noreply.github.com)>
2026-01-23 14:05:57 -0500

    one commit

---
+++ src/nav/nav.tsx
@@ -0,0 +1,36 @@
+import type { JSX } from "react";
+import './css/home.css'
+
+
+export const Nav = (): JSX.Element=>{
+    
+    return(
+        <div className="principal">
+        <div className="nav">
+
+            <h2 className="lander">Lander</h2>
+        <div className="footer">
+            <p className="home">Home</p>
+            <p>About</p>
+            <p>Reviews</p>
+            <p>video</p>
+            <p>Features</p>
+            <p>Products</p>
+            <p>Faqs</p>
+            <p>Contact</p>
+        </div>
+            </div>
+
+        <div className="car">
+            <section className="landingP">
+                <h1>Your Product <br /> Landing page</h1>
+                <p>Write here about your product short description.</p>
+                <button onClick={()=> {console.log('falta el orden now')}}>Order now</button>
+            </section>
+            <section className="content-img">
+            <img className ='img'src="/src/assets/carshop.png" alt="" />
+            </section>
+        </div>
+        </div>
+    )
+};
